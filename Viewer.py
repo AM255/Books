c9d4ab24bb14313a916fe8727748eab9bc58618c
Heart Brake <77528577+AM255@users.noreply.github.com>
2023-01-16 18:09:18 +0900

    Update Viewer.py

--- Viewer.py
+++ Viewer.py
@@ -70,6 +70,60 @@ class BookViewerGUI:
 		# Bind the edit method to a double-click event on the treeview widget
 		self.table.bind("<Double-1>", self.edit)
 
-		# Create a Label widget to display text at the bottom left of the window
-		self.text_label = tk.Label(self.root, text="Credit to Clara Kwin")
-		self.text_label.pack(side="left", anchor="sw")
+	def edit(self, event=None):
+		# Get the selected item in the treeview widget
+		self.item = self.table.focus()
+
+		# Open a new window to edit the data
+		self.edit_window = tk.Toplevel(self.root)
+		self.edit_window.title("Edit")
+
+		# Create labels and entry widgets for each column
+		tk.Label(self.edit_window, text="Title:").grid(row=0, column=0, sticky=tk.W)
+		tk.Label(self.edit_window, text="Author:").grid(row=1, column=0, sticky=tk.W)
+		tk.Label(self.edit_window, text="Language:").grid(row=2,
+		                                                  column=0,
+		                                                  sticky=tk.W)
+		tk.Label(self.edit_window, text="Date:").grid(row=3, column=0, sticky=tk.W)
+		tk.Label(self.edit_window, text="Rating:").grid(row=4, column=0, sticky=tk.W)
+		tk.Label(self.edit_window, text="Publish Year:").grid(row=5,
+		                                                      column=0,
+		                                                      sticky=tk.W)
+		tk.Label(self.edit_window, text="Median Number Pages:").grid(row=6,
+		                                                             column=0,
+		                                                             sticky=tk.W)
+		self.title_entry = tk.Entry(self.edit_window)
+		self.author_entry = tk.Entry(self.edit_window)
+		self.language_entry = tk.Entry(self.edit_window)
+		self.date_entry = tk.Entry(self.edit_window)
+		self.rating_entry = tk.Entry(self.edit_window)
+		self.publish_year_entry = tk.Entry(self.edit_window)
+		self.number_pages_entry = tk.Entry(self.edit_window)
+		self.title_entry.grid(row=0, column=1, padx=5, pady=5)
+		self.author_entry.grid(row=1, column=1, padx=5, pady=5)
+		self.language_entry.grid(row=2, column=1, padx=5, pady=5)
+		self.date_entry.grid(row=3, column=1, padx=5, pady=5)
+		self.rating_entry.grid(row=4, column=1, padx=5, pady=5)
+		self.publish_year_entry.grid(row=5, column=1, padx=5, pady=5)
+		self.number_pages_entry.grid(row=6, column=1, padx=5, pady=5)
+
+		# Pre-populate the entry widgets with the data from the selected row
+		self.title_entry.insert(0, self.table.item(self.item, "values")[0])
+		self.author_entry.insert(0, self.table.item(self.item, "values")[1])
+		self.language_entry.insert(0, self.table.item(self.item, "values")[2])
+		self.date_entry.insert(0, self.table.item(self.item, "values")[3])
+		self.rating_entry.insert(0, self.table.item(self.item, "values")[4])
+		self.publish_year_entry.insert(0, self.table.item(self.item, "values")[5])
+		self.number_pages_entry.insert(0, self.table.item(self.item, "values")[6])
+
+		# Create a save button
+		self.save_button = tk.Button(self.edit_window,
+		                             text="Save",
+		                             command=self.save_edit)
+		self.save_button.grid(row=7, column=0, padx=5, pady=5, sticky=tk.W)
+
+		# Create a cancel button
+		self.cancel_button = tk.Button(self.edit_window,
+		                               text="Cancel",
+		                               command=self.edit_window.destroy)
+		self.cancel_button.grid(row=7, column=1, padx=5, pady=5, sticky=tk.E)
